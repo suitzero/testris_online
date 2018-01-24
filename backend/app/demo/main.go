9063615d6b287ed0112fb77f7a24775bdbbc46e3
suitzero <evildisease@gmail.com>
2018-01-24 12:21:34 -0800

    demo

---
+++ backend/app/demo/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Name      string
+	Phone     string
+	Timestamp time.Time
+}
+
+func main() {
+	url := "mongo"
+
+	const (
+		Username   = "YOUR_USERNAME"
+		Password   = "YOUR_PASS"
+		Database   = "helloworld"
+		Collection = "mydata"
+	)
+
+	session, err := mgo.Dial(url)
+	defer session.Close()
+	if err != nil {
+		panic(err)
+	}
+	c := session.DB(Database).C(Collection)
+
+	// Index
+	index := mgo.Index{
+		Key:        []string{"name", "phone"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	err = c.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
+
+	// Insert Datas
+	err = c.Insert(&Person{Name: "Ale", Phone: "+55 53 1234 4321", Timestamp: time.Now()},
+		&Person{Name: "Cla", Phone: "+66 33 1234 5678", Timestamp: time.Now()})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Query One
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).Select(bson.M{"phone": 0}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Phone", result)
+
+	fmt.Println("vim-go")
+}
